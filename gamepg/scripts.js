1f6d64d9d329b38ec99825b25845bc2a2dafcc35
FreddieThePebble <154924650+FreddieThePebble@users.noreply.github.com>
2025-03-23 09:42:13 +0000

    Update scripts.js

--- gamepg/scripts.js
+++ gamepg/scripts.js
@@ -8,11 +8,8 @@ function openScreenshot(url) {
     window.open(url, '_blank');
 }
 
-// Wait for the page to fully load before checking for screen size
 window.onload = function() {
     if (window.innerWidth <= 768) {
-      // Only show the popup if it's a mobile screen size
       document.getElementById("mobile-popup").style.display = "block";
     }
-  };
-  
+};
